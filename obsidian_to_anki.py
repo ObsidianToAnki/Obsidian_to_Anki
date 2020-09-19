d270a575f54a4d4c538688eca2255ec98ec0a580
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2020-09-19 07:55:14 +0100

    Added wait_for_port snippet

--- obsidian_to_anki.py
+++ obsidian_to_anki.py
@@ -11,6 +11,8 @@ import markdown
 import base64
 import argparse
 import html
+import time
+import socket
 try:
     import gooey
     GOOEY = True
@@ -116,6 +118,30 @@ def findignore(pattern, string, ignore_spans):
         for match in pattern.finditer(string)
         if not overlap(match.span(), ignore_spans)
     )
+
+
+def wait_for_port(port, host='localhost', timeout=5.0):
+    """Wait until a port starts accepting TCP connections.
+    Args:
+        port (int): Port number.
+        host (str): Host address on which the port should exist.
+        timeout (float): In seconds. How long to wait before raising errors.
+    Raises:
+        TimeoutError: The port isn't accepting connection after time specified
+        in `timeout`.
+    """
+    start_time = time.perf_counter()
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=timeout):
+                break
+        except OSError as ex:
+            time.sleep(0.01)
+            if time.perf_counter() - start_time >= timeout:
+                raise TimeoutError(
+                    'Waited too long for the port {} on host {} to'
+                    'start accepting connections.'.format(port, host)
+                ) from ex
 
 
 class AnkiConnect:
