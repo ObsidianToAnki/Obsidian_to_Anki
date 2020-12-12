9c180a03781ddad0f0dd171928be0426e514a783
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2020-12-12 10:09:04 +0000

    Changed scheduling info message

--- src/settings.ts
+++ src/settings.ts
@@ -4,7 +4,7 @@ import * as AnkiConnect from './anki'
 const defaultDescs = {
 	"Tag": "The tag that the plugin automatically adds to any generated cards.",
 	"Deck": "The deck the plugin adds cards to if TARGET DECK is not specified in the file.",
-	"Scheduling Interval": "The time, in minutes, between the vault being scanned automatically. Set this to 0 to disable automatic scanning.",
+	"Scheduling Interval": "The time, in minutes, between automatic scans of the vault. Set this to 0 to disable automatic scanning.",
 	"Add File Link": "Append a link to the file that generated the flashcard on the field specified in the table.",
 	"Add Context": "Append 'context' for the card, in the form of path > heading > heading etc, to the field specified in the table.",
 	"CurlyCloze": "Convert {cloze deletions} -> {{c1::cloze deletions}} on note types that have a 'Cloze' in their name.",
