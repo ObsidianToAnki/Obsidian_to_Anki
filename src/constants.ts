9a0a0d81f4b8fe7989edcd8dcae76da405eb13a7
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2020-12-12 17:45:36 +0000

    Added code syntax highlighting

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,8 @@ export const OBS_DISPLAY_MATH_REGEXP: RegExp = /\$\$([\s\S]*?)\$\$/g
 export const OBS_CODE_REGEXP:RegExp = /(?<!`)`(?=[^`])[\s\S]*?`/g
 export const OBS_DISPLAY_CODE_REGEXP:RegExp = /```[\s\S]*?```/g
 
+export const CODE_CSS_URL = `https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/arta.css`
+
 export function escapeRegex(str: string): string {
     // Got from stackoverflow - https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
     return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
--- src/format.ts
+++ src/format.ts
@@ -140,6 +140,8 @@ export class FormatConverter {
 		let math_matches: string[]
 		let inline_code_matches: string[]
 		let display_code_matches: string[]
+		const add_highlight_css: boolean = note_text.match(c.OBS_DISPLAY_CODE_REGEXP) ? true : false;
+		console.log("ADD HIGHLIGHT: ", add_highlight_css);
 		[note_text, math_matches] = this.censor(note_text, ANKI_MATH_REGEXP, MATH_REPLACE);
 		[note_text, display_code_matches] = this.censor(note_text, c.OBS_DISPLAY_CODE_REGEXP, DISPLAY_CODE_REPLACE);
 		[note_text, inline_code_matches] = this.censor(note_text, c.OBS_CODE_REGEXP, INLINE_CODE_REPLACE);
@@ -155,11 +157,16 @@ export class FormatConverter {
 		note_text = note_text.replace(HIGHLIGHT_REGEXP, String.raw`<mark>$1</mark>`)
 		note_text = this.decensor(note_text, DISPLAY_CODE_REPLACE, display_code_matches)
 		note_text = this.decensor(note_text, INLINE_CODE_REPLACE, inline_code_matches)
+		console.log("HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEY")
+		console.log(note_text)
 		note_text = converter.makeHtml(note_text)
 		note_text = this.decensor(note_text, MATH_REPLACE, math_matches).trim()
 		// Remove unnecessary paragraph tag
 		if (note_text.startsWith(PARA_OPEN) && note_text.endsWith(PARA_CLOSE)) {
 			note_text = note_text.slice(PARA_OPEN.length, -1 * PARA_CLOSE.length)
+		}
+		if (add_highlight_css) {
+			note_text = '<link href="' + c.CODE_CSS_URL + '" rel="stylesheet">' + note_text
 		}
 		return note_text
 	}
