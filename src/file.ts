905cc0c9c39e557e3e1ad287f4120ee993829151
Rubaiyat Khondaker <rubaiyat.khondaker@gmail.com>
2020-12-12 18:26:41 +0000

    Made a new class AllFile, that should be able to handle regexps and START END

--- src/file.ts
+++ src/file.ts
@@ -501,7 +501,6 @@ export class RegexFile extends AbstractFile {
                 this.data.add_context ? this.getContextAtIndex(match.index) : ""
             )
             if (parsed.identifier == CLOZE_ERROR) {
-                //console.log("Note has no cloze deletions!")
                 continue
             }
             parsed.note.tags.push(...this.global_tags.split(TAG_SEP))
@@ -529,4 +528,205 @@ export class RegexFile extends AbstractFile {
     }
 
 
+}
+
+export class AllFile extends AbstractFile {
+    ignore_spans: [number, number][]
+    custom_regexps: Record<string, string>
+    inline_notes_to_add: AnkiConnectNote[]
+    inline_id_indexes: number[]
+    regex_notes_to_add: AnkiConnectNote[]
+    regex_id_indexes: number[]
+
+    constructor(file_contents: string, path:string, url: string, data: FileData, file_cache: CachedMetadata) {
+        super(file_contents, path, url, data, file_cache)
+        this.custom_regexps = data.custom_regexps
+    }
+
+    add_spans_to_ignore() {
+        this.ignore_spans = []
+        this.ignore_spans.push(...spans(this.data.NOTE_REGEXP, this.file))
+        this.ignore_spans.push(...spans(this.data.INLINE_REGEXP, this.file))
+        this.ignore_spans.push(...spans(c.OBS_INLINE_MATH_REGEXP, this.file))
+        this.ignore_spans.push(...spans(c.OBS_DISPLAY_MATH_REGEXP, this.file))
+        this.ignore_spans.push(...spans(c.OBS_CODE_REGEXP, this.file))
+        this.ignore_spans.push(...spans(c.OBS_DISPLAY_CODE_REGEXP, this.file))
+    }
+
+    setupScan() {
+        this.setup_frozen_fields_dict()
+        this.setup_target_deck()
+        this.setup_global_tags()
+        this.add_spans_to_ignore()
+        this.notes_to_add = []
+        this.inline_notes_to_add = []
+        this.regex_notes_to_add = []
+        this.id_indexes = []
+        this.inline_id_indexes = []
+        this.regex_id_indexes = []
+        this.notes_to_edit = []
+        this.notes_to_delete = []
+    }
+
+    scanNotes() {
+        for (let note_match of this.file.matchAll(this.data.NOTE_REGEXP)) {
+            let [note, position]: [string, number] = [note_match[1], note_match.index + note_match[0].indexOf(note_match[1]) + note_match[1].length]
+            // That second thing essentially gets the index of the end of the first capture group.
+            let parsed = new Note(
+                note,
+                this.data.fields_dict,
+                this.data.curly_cloze,
+                this.data.highlights_to_cloze,
+                this.formatter
+            ).parse(
+                this.target_deck,
+                this.url,
+                this.frozen_fields_dict,
+                this.data,
+                this.data.add_context ? this.getContextAtIndex(note_match.index) : ""
+            )
+            if (parsed.identifier == null) {
+                // Need to make sure global_tags get added
+                parsed.note.tags.push(...this.global_tags.split(TAG_SEP))
+                this.notes_to_add.push(parsed.note)
+                this.id_indexes.push(position)
+            } else if (!this.data.EXISTING_IDS.includes(parsed.identifier)) {
+                // Need to show an error
+                if (parsed.identifier == CLOZE_ERROR) {
+                    continue
+                }
+                console.log("Warning! note with id", parsed.identifier, " in file ", this.path, " does not exist in Anki!")
+            } else {
+                this.notes_to_edit.push(parsed)
+            }
+        }
+    }
+
+    scanInlineNotes() {
+        for (let note_match of this.file.matchAll(this.data.INLINE_REGEXP)) {
+            let [note, position]: [string, number] = [note_match[1], note_match.index + note_match[0].indexOf(note_match[1]) + note_match[1].length]
+            // That second thing essentially gets the index of the end of the first capture group.
+            let parsed = new InlineNote(
+                note,
+                this.data.fields_dict,
+                this.data.curly_cloze,
+                this.data.highlights_to_cloze,
+                this.formatter
+            ).parse(
+                this.target_deck,
+                this.url,
+                this.frozen_fields_dict,
+                this.data,
+                this.data.add_context ? this.getContextAtIndex(note_match.index) : ""
+            )
+            if (parsed.identifier == null) {
+                // Need to make sure global_tags get added
+                parsed.note.tags.push(...this.global_tags.split(TAG_SEP))
+                this.inline_notes_to_add.push(parsed.note)
+                this.inline_id_indexes.push(position)
+            } else if (!this.data.EXISTING_IDS.includes(parsed.identifier)) {
+                // Need to show an error
+                if (parsed.identifier == CLOZE_ERROR) {
+                    continue
+                }
+                console.log("Warning! note with id", parsed.identifier, " in file ", this.path, " does not exist in Anki!")
+            } else {
+                this.notes_to_edit.push(parsed)
+            }
+        }
+    }
+
+    search(note_type: string, regexp_str: string) {
+        //Search the file for regex matches
+        //ignoring matches inside ignore_spans,
+        //and adding any matches to ignore_spans.
+        for (let search_id of [true, false]) {
+            for (let search_tags of [true, false]) {
+                console.log("HI")
+                let id_str = search_id ? ID_REGEXP_STR : ""
+                let tag_str = search_tags ? TAG_REGEXP_STR : ""
+                let regexp: RegExp = new RegExp(regexp_str + tag_str + id_str, 'gm')
+                for (let match of findignore(regexp, this.file, this.ignore_spans)) {
+                    this.ignore_spans.push([match.index, match.index + match[0].length])
+                    const parsed: AnkiConnectNoteAndID = new RegexNote(
+                        match, note_type, this.data.fields_dict,
+                        search_tags, search_id, this.data.curly_cloze, this.data.highlights_to_cloze, this.formatter
+                    ).parse(
+                        this.target_deck,
+                        this.url,
+                        this.frozen_fields_dict,
+                        this.data,
+                        this.data.add_context ? this.getContextAtIndex(match.index) : ""
+                    )
+                    if (search_id) {
+                        if (!this.data.EXISTING_IDS.includes(parsed.identifier)) {
+                            if (parsed.identifier == CLOZE_ERROR) {
+                                continue
+                            }
+                            console.log("Warning! Note with id", parsed.identifier, " in file ", this.path, " does not exist in Anki!")
+                        } else {
+                            this.notes_to_edit.push(parsed)
+                        }
+                    } else {
+                        if (parsed.identifier == CLOZE_ERROR) {
+                            continue
+                        }
+                        parsed.note.tags.push(...this.global_tags.split(TAG_SEP))
+                        this.regex_notes_to_add.push(parsed.note)
+                        this.regex_id_indexes.push(match.index + match[0].length)
+                    }
+                }
+            }
+        }
+    }
+
+    scanFile() {
+        this.setupScan()
+        this.scanNotes()
+        this.scanInlineNotes()
+        for (let note_type in this.custom_regexps) {
+            const regexp_str: string = this.custom_regexps[note_type]
+            if (regexp_str) {
+                this.search(note_type, regexp_str)
+            }
+        }
+        this.all_notes_to_add = this.notes_to_add.concat(this.inline_notes_to_add).concat(this.regex_notes_to_add)
+        this.scanDeletions()
+    }
+
+    fix_newline_ids() {
+        this.file = this.file.replace(double_regexp, "$1")
+    }
+
+    writeIDs() {
+        let normal_inserts: [number, string][] = []
+        this.id_indexes.forEach(
+            (id_position: number, index: number) => {
+                const identifier: number | null = this.note_ids[index]
+                if (identifier) {
+                    normal_inserts.push([id_position, id_to_str(identifier, false, this.data.comment)])
+                }
+            }
+        )
+        let inline_inserts: [number, string][] = []
+        this.inline_id_indexes.forEach(
+            (id_position: number, index: number) => {
+                const identifier: number | null = this.note_ids[index + this.notes_to_add.length] //Since regular then inline
+                if (identifier) {
+                    inline_inserts.push([id_position, id_to_str(identifier, true, this.data.comment)])
+                }
+            }
+        )
+        let regex_inserts: [number, string][] = []
+        this.regex_id_indexes.forEach(
+            (id_position: number, index: number) => {
+                const identifier: number | null = this.note_ids[index + this.notes_to_add.length + this.inline_notes_to_add.length] // Since regular then inline then regex
+                if (identifier) {
+                    regex_inserts.push([id_position, "\n" + id_to_str(identifier, false, this.data.comment)])
+                }
+            }
+        )
+        this.file = string_insert(this.file, normal_inserts.concat(inline_inserts).concat(regex_inserts))
+        this.fix_newline_ids()
+    }
 }
