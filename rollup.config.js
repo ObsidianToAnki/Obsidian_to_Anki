7312a005edf315ed4abf10d9eed99da1fe67041e
niposch <niposch@gmail.com>
2022-02-13 15:49:31 +0100

    Style.css and manifest are copied to build output folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,28 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve'
+import {
+  nodeResolve
+} from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import nodePolyfills from 'rollup-plugin-node-polyfills'
+import copy from "rollup-plugin-copy-assets";
 
 export default {
   input: 'main.ts',
   output: {
-    file: 'main.js',
+    file: './build/main.js',
     format: 'cjs',
     exports: "default"
   },
-  plugins: [nodeResolve(), commonjs(), typescript()],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript(),
+    copy({
+      assets: [
+        "./manifest.json",
+        "./styles.css",
+      ],
+    }),
+  ],
   external: ["obsidian", "path"]
 };
