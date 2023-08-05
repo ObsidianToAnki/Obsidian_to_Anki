c3cb516f673a774b4196d621817cf70adecaa76a
Lukas Raab <MisterPresident@users.noreply.github.com>
2023-08-05 22:36:18 +0200

    Update files-manager.ts fix Pseudonium/Obsidian_to_Anki#408

--- src/files-manager.ts
+++ src/files-manager.ts
@@ -303,7 +303,10 @@ export class FileManager {
         temp = []
         console.info("Requesting tags to be replaced...")
         for (let file of this.ownFiles) {
-            temp.push(file.getClearTags())
+             let rem = file.getClearTags()
+             if(rem.params.notes.length) {
+                  temp.push(rem))
+             }
         }
         requests.push(AnkiConnect.multi(temp))
         temp = []
